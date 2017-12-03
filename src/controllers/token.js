09463ce5f10a9c33f5c50de0e3495c88dd35f0d1
Gabriel Santos da Rosa <gabrielsantoscimol@gmail.com>
2017-12-03 13:38:23 -0200

    Ajuste nas validações do token

--- src/controllers/token.js
+++ src/controllers/token.js
@@ -4,25 +4,42 @@ export default class Controller {
   updateToken(req, res) {
     const oldToken = req.body.oldToken
     const newToken = req.body.newToken
-    if (!oldToken || !newToken) {
+    if (!newToken) {
       res.end('Informe os tokens.')
       return
     }
-    Token.findOne({ where: { token: oldToken } })
-      .then(token => {
-        if (token)
-          Token.update({ token: newToken }, { where: { token: oldToken } })
-            .then(() => {
-              res.end('Token atualizado.')
-            }).catch((e) => { throw e })
-        else
-          Token.create({ token: newToken })
-            .then(() => {
-              res.end('Token criado.')
-            }).catch((e) => { throw e })
-      })
-      .catch(err => {
-        res.end('ItemController.getAll() => ' + err.message)
-      })
+
+    if (oldToken) {
+      Token.findOne({ where: { token: oldToken } })
+        .then(token => {
+          if (token)
+            Token.update({ token: newToken }, { where: { token: oldToken } })
+              .then(() => {
+                res.end('Token atualizado.')
+              }).catch((e) => { throw e })
+          else
+            Token.create({ token: newToken })
+              .then(() => {
+                res.end('Token criado.')
+              }).catch((e) => { throw e })
+        })
+        .catch(err => {
+          res.end('ItemController.getAll() => ' + err.message)
+        })
+    } else {
+      Token.findOne({ where: { token: newToken } })
+        .then(token => {
+          if (token)
+                res.end('O token já está atualizado.')
+          else
+            Token.create({ token: newToken })
+              .then(() => {
+                res.end('Token criado.')
+              }).catch((e) => { throw e })
+        })
+        .catch(err => {
+          res.end('ItemController.getAll() => ' + err.message)
+        })
+    }
   }
 }
